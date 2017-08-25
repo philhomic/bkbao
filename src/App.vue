00fbcf6b2ac2a8871908ff9174cff6cd88faf84d
philhomic <philhomic@qq.com>
2017-08-25 16:57:48 +0800

    bkbao just a small start

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app id="example-3" toolbar>
+      <v-navigation-drawer permanent clipped light>
+        <v-list dense class="pt-0">
+          <v-list-tile v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar class="teal" dark>
+        <v-toolbar-title>Toolbar</v-toolbar-title>
+      </v-toolbar>
+      <main>
+        <v-container fluid>
+          <!--v-router-->
+        </v-container>
+      </main>
+    </v-app>
+  </template>
+
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
